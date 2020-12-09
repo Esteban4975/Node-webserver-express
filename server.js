6d30eb0e462f53a7df5f884efc584827ffd1d4ce
Esteban <esteban.vame@hotmail.com>
2020-12-08 18:59:40 -0600

    Primer commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT||3000;
+
+
+app.use(express.static(__dirname+'/public'));
+
+// Express HBS engine
+hbs.registerPartials(__dirname+'/views/parciales/');
+app.set('view engine','hbs');
+
+/* app.get('/',function(req,res){
+    //res.send('Hola mundo');
+    let salida = {
+        nombre: 'Esteban',
+        edad: 32,
+        url: req.url
+    }
+    res.send(salida);
+}) */
+
+app.get('/',function(req,res){
+
+    res.render('home',{
+        nombre: 'esteban',
+        anio: new Date().getFullYear()
+    });
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        nombre: 'Esteban',
+        anio: new Date().getFullYear()
+    });
+})
+
+app.listen(port, ()=>{
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+})
